349142cb4ade5cfade64378e21a469934e9e71df
Lieu Tri <lieutri98@gmail.com>
2024-01-15 22:19:50 +0700

    add pick image

--- src/js/main.js
+++ src/js/main.js
@@ -196,3 +196,31 @@ $('#pinkboard').click(function () {
 // });
 
 update();
+
+////////////////////////////
+
+function changePicture(file) {
+
+}
+
+$(function(){
+  $('#changepicture').change(function(){
+    var input = this;
+    var url = $(this).val();
+    var ext = url.substring(url.lastIndexOf('.') + 1).toLowerCase();
+    if (input.files && input.files[0]&& (ext == "gif" || ext == "png" || ext == "jpeg" || ext == "jpg")) 
+     {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+           $('#img').attr('src', e.target.result);
+        }
+       reader.readAsDataURL(input.files[0]);
+    }
+    else
+    {
+      $('#img').attr('src', '/src/image/CRUSH.jpg');
+    }
+  });
+
+});
